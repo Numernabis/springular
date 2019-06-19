b12bdc0e0502f5c1595d05a7800633fa92a2ebfa
Numernabis <luigi.zielone@gmail.com>
2019-06-20 01:00:03 +0200

    Add bootstrap and change proxy

--- server/src/main/java/com/example/springular/CampaignController.java
+++ server/src/main/java/com/example/springular/CampaignController.java
@@ -7,8 +7,8 @@ import java.util.List;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-//@RequestMapping({"/api"})
-@RequestMapping({"/campaigns"})
+@RequestMapping({"/api"})
+//@RequestMapping({"/campaigns"})
 public class CampaignController {
 
     @Autowired
