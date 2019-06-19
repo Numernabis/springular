c536857fe312194a5a8b1d8e2e3e6c1b06551c01
Numernabis <luigi.zielone@gmail.com>
2019-06-20 00:49:17 +0200

    Update app module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CampaignComponent } from './campaign/campaign.component';
+import {AppComponent} from './app.component';
+import {CampaignComponent} from './campaign/campaign.component';
+import {AppRoutingModule} from './app-routing.module';
+import {CampaignService} from './campaign/campaign.service';
+import {HttpClientModule} from "@angular/common/http";
+import {AddCampaignComponent} from './campaign/add-campaign.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CampaignComponent
+    CampaignComponent,
+    AddCampaignComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [CampaignService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
